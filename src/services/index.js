9cd23a0cfd6fbffc106b5a393806dc28a2b2e13e
JereUR <jeremias.jdv@gmail.com>
2023-09-07 21:11:56 -0300

    feat(Root): Functions added: get user, sign in with Google and Github and sign out

--- src/services/index.js
+++ src/services/index.js
@@ -1,27 +1,51 @@
 import { supabase } from './supabase'
 
+export const getUserFromSupabase = async () => {
+  return await supabase.auth.getUser()
+}
+
 export const getIncomesFromSupabase = async () => {
-  const user = await supabase.auth.getSession()
+  const user = await getUserFromSupabase()
 
   if (user === null) return
 
   const { data: incomes, error } = await supabase
     .from('incomes')
     .select('*')
-    .eq('user_id', user.id)
+    .eq('user_id', user.data.user.id)
 
   return [error, incomes]
 }
 
 export const getExpensesFromSupabase = async () => {
-  const user = await supabase.auth.getSession()
+  const user = await getUserFromSupabase()
 
   if (user === null) throw new Error({ message: 'Sesión no iniciada.' })
 
   const { data: expenses, error } = await supabase
     .from('expenses')
     .select('*')
-    .eq('user_id', user.id)
+    .eq('user_id', user.data.user.id)
 
   return [error, expenses]
+}
+
+export const signInWithGoogle = async () => {
+  const { user, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google'
+  })
+
+  return [user, error]
+}
+
+export const signInWithGithub = async () => {
+  const { user, error } = await supabase.auth.signInWithOAuth({
+    provider: 'github'
+  })
+
+  return [user, error]
+}
+
+export const signOutFromSupabase = () => {
+  supabase.auth.signOut()
 }

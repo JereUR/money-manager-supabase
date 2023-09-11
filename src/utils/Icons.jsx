e354998def223dc2f000ac83c292bce5648ffb0b
JereUR <jeremias.jdv@gmail.com>
2023-09-10 21:08:43 -0300

    feat(Root): Back arrow icon added

--- src/utils/Icons.jsx
+++ src/utils/Icons.jsx
@@ -67,3 +67,18 @@ export const githubIcon = (
     </g>
   </svg>
 )
+
+export const arrowBack = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="30"
+    height="30"
+  >
+    <path fill="none" d="M0 0h24v24H0z"></path>
+    <path
+      fill="currentColor"
+      d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z"
+    ></path>
+  </svg>
+)
